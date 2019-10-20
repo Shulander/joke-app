b72bb92cc02d6e3e6b2f9f768d6a0f782f265559
Henrique Vicentini <henrique@vicentini.us>
2019-10-19 20:18:14 -0700

    Class 05.81 Spring XML Configuration example

--- src/main/java/us/vicentini/jokeapp/JokeApplication.java
+++ src/main/java/us/vicentini/jokeapp/JokeApplication.java
@@ -2,8 +2,10 @@ package us.vicentini.jokeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeApplication {
 
     public static void main(String[] args) {
--- src/main/java/us/vicentini/jokeapp/config/SpringBeansConfiguration.java
+++ src/main/java/us/vicentini/jokeapp/config/SpringBeansConfiguration.java
@@ -1,13 +1,11 @@
 package us.vicentini.jokeapp.config;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 public class SpringBeansConfiguration {
 
-    @Bean
+//    @Bean
     public ChuckNorrisQuotes chuckNorrisQuotes(){
         return new ChuckNorrisQuotes();
     }
